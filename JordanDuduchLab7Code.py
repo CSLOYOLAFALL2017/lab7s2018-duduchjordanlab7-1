dfa8d98e5f90358ceeae8ebf7766bd9f7a47d224
timjordan03 <35737112+timjordan03@users.noreply.github.com>
2018-03-23 12:58:40 -0400

    Add files via upload

--- JordanDuduchLab7Code.py
+++ JordanDuduchLab7Code.py
@@ -1,13 +1,13 @@
-#
-#
-#
-#
-#
+# Tim Jordan & Tim Duduch
+# CS151.02
+# Franceschi
 #
 #
 #
 
 import os
+import sys
+
 
 def movie_file():
     filename = input("Please input a file name: ")
@@ -21,21 +21,54 @@ def movie_file():
 def maximum_profit(filename):
 
     try:
-        file_object = open(filename, "r")
-        print( "File opened for reading" )
-        for line in file_object:
-            print(line, end = " ")
+        filename = open(filename, "r")
+        profit = 0
+        bestmovie = ""
+
+        for line in filename:
+
             date, title, budget, revenue = line.split(",")
-            budget = int(budget)
-            revenue = int(revenue)
-
-
-
+            budget = float(budget)
+            revenue = float(revenue)
+            totalRevenue = revenue - budget
+            if totalRevenue > profit:
+                profit = totalRevenue
+                bestmovie = title
+        print("The movie with the highest profit was:", bestmovie, "who's profit was $%.2f" % profit)
     except FileNotFoundError:
-        print( "File does not exist" )
+        print("File does not exist")
+        sys.exit()
+    return
 
 
+def outputFile(newFile, openFile):
+    newOutput = open(openFile, "w")
+
+    try:
+        newFile = open(newFile, "r")
+        for line in newFile:
+            date, title, budget, revenue = line.split(",")
+            budget = float(budget)
+            revenue = float(revenue)
+            totalRevenue = revenue - budget
+            print("Date Released:", date, file = newOutput)
+            print("Title:", title, file = newOutput)
+            print("Total Revenue: $%2f" % revenue, file = newOutput)
+    except FileNotFoundError:
+        print("File does not exist")
+        sys.exit()
+    return
+
+def main():
+    print("This program is designed to output the movie with the highest profit.")
+    newFile = movie_file()
+    openFile = input ("Please enter a file to output to:")
+
+    maximum_profit(newFile)
+
+    outputFile(newFile, openFile)
+
+    return
 
 
-movies = movie_file()
-maximum_profit(movies)
+main()
